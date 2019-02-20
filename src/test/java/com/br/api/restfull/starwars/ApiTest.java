db2ee7fcd77f69629e61ff780c495055cd399838
Silvio <silvio0407@gmail.com>
2019-02-20 15:55:44 -0300

    ApiTest atualizada com pesquisa dos planetas na API SWAPI.

--- src/test/java/com/br/api/restfull/starwars/ApiTest.java
+++ src/test/java/com/br/api/restfull/starwars/ApiTest.java
@@ -11,38 +11,49 @@ public class ApiTest {
 
 	public static void main(String[] args) throws UnirestException, JSONException {
 		
-		//https://swapi.co/api/planets/?search=
+		listarPlanetaApiSwapi();
+            
+	}
+	
+	private static void listarPlanetaApiSwapi(){
 		
-            String planetaApi = Unirest.get("https://swapi.co/api/planets/?search={name}")
-            		.routeParam("name", "Naboo")
-                    .asJson()
-                    .getBody()
-                    .toString();
-            
-            JSONObject devMovies = new JSONObject(planetaApi);
-            
-            String nextPag = devMovies.get("next").toString();
-            
-            String count = devMovies.get("count").toString();
-            
-            JSONArray arrFilmes = devMovies.getJSONArray("results");
-            
-            //JSONObject result = devMovies.getJSONObject("results");
-            
-            //JSONArray arrFilmes = devMovies.getJSONArray("films");
-            
-            for (int i=0; i < arrFilmes.length(); i++) {
-                
-                JSONObject f = arrFilmes.getJSONObject(i);
-                
-                JSONArray fms = f.getJSONArray("films");
-                 
-                //System.out.println("id: " + f.getInt("id"));
-                System.out.println("name: " + f.getString("name"));
-                System.out.println("climate: " + f.getString("climate"));
-                System.out.println("terrain: " + f.getString("terrain"));
-            }
-            
+		String urlApiSwapi = "https://swapi.co/api/planets/";
+		
+		boolean hasNextPage = true;
+		try {
+			
+			while(hasNextPage){
+				
+				String planetasApi = Unirest.get(urlApiSwapi)
+				        .asJson()
+				        .getBody()
+				        .toString();
+				
+				JSONObject planetaJson = new JSONObject(planetasApi);
+				
+				urlApiSwapi = planetaJson.get("next").toString();
+				
+				hasNextPage = hasMorePage(urlApiSwapi);
+				
+				JSONArray results = planetaJson.getJSONArray("results");
+				
+				for(int i = 0; i < results.length(); i++ ){
+					JSONObject resultJsonObject = results.getJSONObject(i);
+					
+					System.out.println("name: " + i + " - "+  resultJsonObject.getString("name"));
+				}
+			}
+			
+		} catch (UnirestException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static boolean hasMorePage(String nextPag){
+		return nextPag.isEmpty() ? false : true;
 	}
 
 }
